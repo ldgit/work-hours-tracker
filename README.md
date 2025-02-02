5ca5a25d3aaab55082dcb144c535ba270e004e0c
Danko Lučić <ld.is.a.git@gmail.com>
2025-02-02 20:55:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Work Hours Tracker
+
+🚨🚧 WIP 🚧🚨
+
+Track your working hours without sign-ups or sending data to some server.
+
+Planned Features:
+- Fully client-side: all data is stored locally on user's machine
+- Create backups your working hours data
+
---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import Button from './lib/Button.svelte';
+</script>
+
+<main>
+  <h1>Work hours tracker</h1>
+
+  <div>
+    <Button onclick={() => console.log('Start tracking')}>Start tracking!</Button>
+  </div>
+</main>
+
+<style>
+</style>
