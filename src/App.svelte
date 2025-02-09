faa5ea6a322efe0fe65ca0aa40d1c3cb688b8592
Danko Lučić <ld.is.a.git@gmail.com>
2025-02-09 18:35:16 +0100

    extract user form into its own component

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
 	import { getDatabase } from "./lib/database";
-	import Button from "./ui/Button.svelte";
+	import UserForm from "./ui/UserForm.svelte";
 
-	let username = $state("");
-	let paidBreakDuration = $state(45);
 	async function getUserCount() {
 		const db = await getDatabase();
 		const userCount = await db.getUserCount();
@@ -17,57 +15,7 @@
 
 	{#await getUserCount() then userCount}
 		{#if userCount === 0}
-			<form
-				onsubmit={async (e) => {
-					(await getDatabase()).insertUser({
-						username,
-						paidBreakDuration,
-					});
-				}}
-			>
-				<div class="inputRow">
-					<label>
-						<span>Username</span>
-						<input
-							bind:value={username}
-							placeholder="username"
-							required
-							type="text"
-						/>
-					</label>
-				</div>
-				<div class="inputRow">
-					<label>
-						<span title="Union mandated!">Daily paid break</span>
-						<input
-							bind:value={paidBreakDuration}
-							type="number"
-							placeholder="in minutes"
-							required
-						/>
-					</label>
-				</div>
-				<div>
-					<Button type="submit">Start tracking!</Button>
-				</div>
-			</form>
+			<UserForm />
 		{/if}
 	{/await}
 </main>
-
-<style>
-	.inputRow {
-		margin-bottom: 0.4rem;
-	}
-
-	label {
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-	}
-
-	input {
-		padding: 4px;
-		margin: 4px;
-	}
-</style>
---
+++ src/ui/UserForm.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+	import { getDatabase } from "../lib/database";
+	import Button from "./Button.svelte";
+
+	let username = $state("");
+	let paidBreakDuration = $state(45);
+</script>
+
+<form
+	onsubmit={async () => {
+		(await getDatabase()).insertUser({
+			username,
+			paidBreakDuration,
+		});
+	}}
+>
+	<div class="inputRow">
+		<label>
+			<span>Username</span>
+			<input
+				bind:value={username}
+				placeholder="username"
+				required
+				type="text"
+			/>
+		</label>
+	</div>
+	<div class="inputRow">
+		<label>
+			<span title="Union mandated!">Daily paid break</span>
+			<input
+				bind:value={paidBreakDuration}
+				type="number"
+				placeholder="in minutes"
+				required
+			/>
+		</label>
+	</div>
+	<div>
+		<Button type="submit">Start tracking!</Button>
+	</div>
+</form>
+
+<style>
+	.inputRow {
+		margin-bottom: 0.4rem;
+	}
+
+	label {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+
+	input {
+		padding: 4px;
+		margin: 4px;
+	}
+</style>
