fbd84a66803014addd6999571ebf2d8983f2266b
Danko Lučić <ld.is.a.git@gmail.com>
2025-02-15 21:39:46 +0100

    work hours tracker core logic

--- src/lib/tracker.test.ts
+++ src/lib/tracker.test.ts
@@ -1,11 +1,582 @@
-import { expect, test } from "vitest";
-import { createTracker } from "./tracker";
+import { beforeEach, afterEach, expect, test, vi } from "vitest";
+import {
+	createTracker,
+	type TimeWorked,
+	type UserSettings,
+	type Workday,
+} from "./tracker";
+import { subHours, subMinutes, subSeconds } from "date-fns";
 
-test("can create tracker", () => {
-	expect(
-		createTracker(
-			{ id: "", username: "Mark S.", paidBreakDuration: 45 },
-			{ workdays: [] },
-		),
-	).not.toBeFalsy();
+let defaultUserSettings: UserSettings;
+
+beforeEach(() => {
+	defaultUserSettings = {
+		id: "66734552-01fb-4663-9c91-27a84ff07efa",
+		username: "Mark S.",
+		paidBreakDuration: 45,
+	};
+});
+
+afterEach(() => {
+	vi.useRealTimers();
+});
+
+test("tracker can start the workday", () => {
+	const tracker = createTracker(defaultUserSettings, { workdays: [] });
+	expect(tracker.hasWorkdayStarted()).toStrictEqual(false);
+
+	expect(tracker.startWorkday());
+
+	expect(tracker.hasWorkdayStarted()).toStrictEqual(true);
+	expect(tracker.hasBreakStarted()).toStrictEqual(false);
+});
+
+test("tracker can start a break", () => {
+	const currentDate = new Date();
+	const tracker = createTracker(defaultUserSettings, {
+		workdays: [
+			{
+				events: [{ type: "start-workday", time: subHours(currentDate, 2) }],
+				paidBreakDuration: 35,
+			},
+		],
+	});
+
+	tracker.startBreak();
+
+	expect(tracker.hasBreakStarted()).toStrictEqual(true);
+	expect(tracker.hasWorkdayStarted()).toStrictEqual(true);
+});
+
+test("tracker cannot start a break if workday has not started", () => {
+	const currentDate = new Date();
+	const tracker = createTracker(defaultUserSettings, {
+		workdays: [
+			{
+				events: [
+					{ type: "start-workday", time: subHours(currentDate, 2) },
+					{ type: "end-workday", time: currentDate },
+				],
+				paidBreakDuration: 35,
+			},
+		],
+	});
+
+	expect(() => tracker.startBreak()).toThrowError(
+		new Error("Workday has not started."),
+	);
+});
+
+(
+	[
+		{ description: "fresh tracking data", workdays: [] },
+		{
+			description: "workday started",
+			workdays: [
+				{
+					events: [{ type: "start-workday", time: subHours(new Date(), 2) }],
+					paidBreakDuration: 35,
+				},
+			],
+		},
+		{
+			description: "workday ended",
+			workdays: [
+				{
+					events: [
+						{ type: "start-workday", time: subHours(new Date(), 2) },
+						{ type: "end-workday", time: new Date() },
+					],
+					paidBreakDuration: 35,
+				},
+			],
+		},
+		{
+			description: "break ended",
+			workdays: [
+				{
+					events: [
+						{ type: "start-workday", time: subHours(new Date(), 2) },
+						{ type: "start-break", time: subHours(new Date(), 1) },
+						{ type: "end-break", time: new Date() },
+					],
+					paidBreakDuration: 35,
+				},
+			],
+		},
+	] as { description: string; workdays: Workday[] }[]
+).forEach(({ description, workdays }) => {
+	test(`tracker cannot end a break if one has not started (${description})`, () => {
+		const tracker = createTracker(defaultUserSettings, { workdays });
+
+		expect(() => tracker.endBreak()).toThrowError(
+			new Error("Cannot end the break if a break has not started."),
+		);
+	});
+});
+
+test("tracker can end a break", () => {
+	const currentDate = new Date();
+	const tracker = createTracker(defaultUserSettings, {
+		workdays: [
+			{
+				events: [{ type: "start-workday", time: subHours(currentDate, 2) }],
+				paidBreakDuration: 35,
+			},
+		],
+	});
+	tracker.startBreak();
+
+	tracker.endBreak();
+
+	expect(tracker.hasBreakStarted()).toStrictEqual(false);
+});
+
+test("workday can last after midnight the next day ", () => {
+	const currentDate = new Date();
+	const tracker = createTracker(defaultUserSettings, {
+		workdays: [
+			{
+				events: [{ type: "start-workday", time: subHours(currentDate, 24) }],
+				paidBreakDuration: 35,
+			},
+		],
+	});
+	expect(tracker.hasWorkdayStarted()).toStrictEqual(true);
+
+	tracker.endWorkday();
+
+	expect(tracker.hasWorkdayStarted()).toStrictEqual(false);
+});
+
+test("tracker cannot start a new workday if previous one has not ended", () => {
+	const currentDate = new Date();
+	const tracker = createTracker(defaultUserSettings, {
+		workdays: [
+			{
+				events: [{ type: "start-workday", time: subHours(currentDate, 2) }],
+				paidBreakDuration: 35,
+			},
+		],
+	});
+
+	expect(() => tracker.startWorkday()).toThrowError(
+		new Error("Cannot start workday if current workday has not ended."),
+	);
+});
+
+(
+	[
+		{ description: "fresh tracking data", workdays: [] },
+		{
+			description: "last workday ended",
+			workdays: [
+				{
+					events: [
+						{ type: "start-workday", time: subHours(new Date(), 2) },
+						{ type: "end-workday", time: new Date() },
+					],
+					paidBreakDuration: 35,
+				},
+			],
+		},
+	] as { description: string; workdays: Workday[] }[]
+).forEach(({ description, workdays }) => {
+	test(`tracker cannot end a workday that has not started (${description})`, () => {
+		const tracker = createTracker(defaultUserSettings, { workdays });
+
+		expect(() => tracker.endWorkday()).toThrowError(
+			new Error("Cannot end the workday because it has not started."),
+		);
+	});
+});
+
+test("tracker can end the workday", () => {
+	const currentDate = new Date();
+	const tracker = createTracker(defaultUserSettings, {
+		workdays: [
+			{
+				events: [{ type: "start-workday", time: subHours(currentDate, 2) }],
+				paidBreakDuration: 35,
+			},
+		],
+	});
+	expect(tracker.hasWorkdayStarted()).toStrictEqual(true);
+
+	tracker.endWorkday();
+
+	expect(tracker.hasWorkdayStarted()).toStrictEqual(false);
+});
+
+(
+	[
+		{
+			description: "fresh tracking data",
+			workdays: [],
+			expected: { hours: 0, minutes: 0, seconds: 0 },
+		},
+		{
+			description: "last workday ended",
+			workdays: [
+				{
+					events: [
+						{ type: "start-workday", time: subHours(new Date(), 2) },
+						{ type: "end-workday", time: new Date() },
+					],
+					paidBreakDuration: 35,
+				},
+			],
+			expected: { hours: 2, minutes: 0, seconds: 0 },
+		},
+		{
+			description: "workday still in progress, no breaks",
+			workdays: [
+				{
+					events: [{ type: "start-workday", time: subHours(new Date(), 2) }],
+					paidBreakDuration: 35,
+				},
+			],
+			expected: { hours: 2, minutes: 0, seconds: 0 },
+		},
+		{
+			description: "without breaks",
+			workdays: [
+				{
+					events: [
+						{
+							type: "start-workday",
+							time: subSeconds(subMinutes(subHours(new Date(), 2), 33), 12),
+						},
+						{ type: "end-workday", time: new Date() },
+					],
+					paidBreakDuration: 35,
+				},
+			],
+			expected: { hours: 2, minutes: 33, seconds: 12 },
+		},
+		{
+			description: "1 one 24 minutes and 35 seconds break, no paid break",
+			workdays: [
+				{
+					events: [
+						// Workday lasted 4 hours
+						{ type: "start-workday", time: subHours(new Date(), 4) },
+						{ type: "start-break", time: subHours(new Date(), 2) },
+						// Break lasted 24 minutes and 35 seconds
+						{
+							type: "end-break",
+							time: subSeconds(subMinutes(subHours(new Date(), 1), 35), 25),
+						},
+						{ type: "end-workday", time: new Date() },
+					],
+					paidBreakDuration: 0,
+				},
+			],
+			expected: { hours: 3, minutes: 35, seconds: 25 },
+		},
+		{
+			description: "One 30 minute break, 45 min paid break",
+			workdays: [
+				{
+					events: [
+						// Workday lasted 4 hours
+						{ type: "start-workday", time: subHours(new Date(), 4) },
+						{ type: "start-break", time: subHours(new Date(), 2) },
+						{
+							type: "end-break",
+							time: subMinutes(subHours(new Date(), 1), 30),
+						},
+						{ type: "end-workday", time: new Date() },
+					],
+					paidBreakDuration: 45,
+				},
+			],
+			expected: { hours: 4, minutes: 0, seconds: 0 },
+		},
+		{
+			description: "Break currently in progress, no paid break",
+			workdays: [
+				{
+					events: [
+						{ type: "start-workday", time: subHours(new Date(), 4) },
+						{ type: "start-break", time: subHours(new Date(), 1) },
+					],
+					paidBreakDuration: 0,
+				},
+			],
+			expected: { hours: 3, minutes: 0, seconds: 0 },
+		},
+		{
+			description: "Break currently in progress, 30 minute paid break",
+			workdays: [
+				{
+					events: [
+						{ type: "start-workday", time: subHours(new Date(), 4) },
+						{ type: "start-break", time: subMinutes(new Date(), 30) },
+					],
+					paidBreakDuration: 30,
+				},
+			],
+			expected: { hours: 4, minutes: 0, seconds: 0 },
+		},
+		{
+			description: "Break currently in progress, 20 minute paid break",
+			workdays: [
+				{
+					events: [
+						{ type: "start-workday", time: subHours(new Date(), 4) },
+						{ type: "start-break", time: subMinutes(new Date(), 30) },
+					],
+					paidBreakDuration: 20,
+				},
+			],
+			expected: { hours: 3, minutes: 50, seconds: 0 },
+		},
+		{
+			description:
+				"30 minute break currently in progress, 45 minute paid break",
+			workdays: [
+				{
+					events: [
+						{ type: "start-workday", time: subHours(new Date(), 4) },
+						{ type: "start-break", time: subMinutes(new Date(), 30) },
+					],
+					paidBreakDuration: 45,
+				},
+			],
+			expected: { hours: 4, minutes: 0, seconds: 0 },
+		},
+		{
+			description:
+				"One 1 hour 24 minutes and 35 seconds break, 30 minute paid break",
+			workdays: [
+				{
+					events: [
+						// Workday lasted 4 hours
+						{ type: "start-workday", time: subHours(new Date(), 4) },
+						{ type: "start-break", time: subHours(new Date(), 2) },
+						// Break lasted 24 minutes and 35 seconds
+						{
+							type: "end-break",
+							time: subSeconds(subMinutes(subHours(new Date(), 1), 35), 25),
+						},
+						{ type: "end-workday", time: new Date() },
+					],
+					paidBreakDuration: 30,
+				},
+			],
+			expected: { hours: 4, minutes: 0, seconds: 0 },
+		},
+		{
+			description:
+				"One half-hour break, workday not ended, 45 minute paid break",
+			workdays: [
+				{
+					events: [
+						{ type: "start-workday", time: subHours(new Date(), 4) },
+						{ type: "start-break", time: subHours(new Date(), 2) },
+						{
+							type: "end-break",
+							time: subMinutes(subHours(new Date(), 1), 30),
+						},
+					],
+					paidBreakDuration: 45,
+				},
+			],
+			expected: { hours: 4, minutes: 0, seconds: 0 },
+		},
+		{
+			description:
+				"One 1 hour 24 minutes and 35 seconds break, 10 minute paid break",
+			workdays: [
+				{
+					events: [
+						// Workday lasted 4 hours
+						{ type: "start-workday", time: subHours(new Date(), 4) },
+						{ type: "start-break", time: subHours(new Date(), 2) },
+						// Break lasted 24 minutes and 35 seconds
+						{
+							type: "end-break",
+							time: subSeconds(subMinutes(subHours(new Date(), 1), 35), 25),
+						},
+						{ type: "end-workday", time: new Date() },
+					],
+					paidBreakDuration: 10,
+				},
+			],
+			expected: { hours: 3, minutes: 45, seconds: 25 },
+		},
+		{
+			description: "Multiple breaks, 10 minute paid break",
+			workdays: [
+				{
+					events: [
+						// Workday lasted 4 hours
+						{ type: "start-workday", time: subHours(new Date(), 4) },
+						// Break lasts 24 minutes and 35 seconds
+						{ type: "start-break", time: subHours(new Date(), 2) },
+						{
+							type: "end-break",
+							time: subSeconds(subMinutes(subHours(new Date(), 1), 35), 25),
+						},
+						// Break lasts 10 minutes and 24 seconds
+						{
+							type: "start-break",
+							time: subSeconds(subMinutes(new Date(), 35), 25),
+						},
+						{
+							type: "end-break",
+							time: subSeconds(subMinutes(new Date(), 25), 1),
+						},
+						{ type: "end-workday", time: new Date() },
+					],
+					paidBreakDuration: 10,
+				},
+			],
+			expected: { hours: 3, minutes: 35, seconds: 1 },
+		},
+	] as { description: string; workdays: Workday[]; expected: TimeWorked }[]
+).forEach(({ description, workdays, expected }) => {
+	test(`get time worked since workday started (${description})`, () => {
+		const tracker = createTracker(defaultUserSettings, { workdays });
+
+		expect(tracker.getTimeWorked()).toEqual(expected);
+	});
+});
+
+test(`getTimeWorked only returns time worked for last workday`, () => {
+	const tracker = createTracker(defaultUserSettings, {
+		workdays: [
+			{
+				events: [
+					// Workday lasted 8 hours
+					{ type: "start-workday", time: subHours(new Date(), 8) },
+					{ type: "end-workday", time: new Date() },
+				],
+				paidBreakDuration: 5,
+			},
+			// We measure this workday.
+			{
+				events: [
+					// Workday lasted 4 hours
+					{ type: "start-workday", time: subHours(new Date(), 4) },
+					// 10 minute break
+					{ type: "start-break", time: subMinutes(new Date(), 35) },
+					{ type: "end-break", time: subMinutes(new Date(), 25) },
+					{ type: "end-workday", time: new Date() },
+				],
+				paidBreakDuration: 5,
+			},
+		],
+	});
+
+	expect(tracker.getTimeWorked()).toEqual({
+		hours: 3,
+		minutes: 55,
+		seconds: 0,
+	});
+});
+
+test("trackers can exchange tracking data", () => {
+	const oldTracker = createTracker(defaultUserSettings, { workdays: [] });
+	expect(oldTracker.startWorkday());
+
+	const newTracker = createTracker(
+		defaultUserSettings,
+		oldTracker.getTrackingData(),
+	);
+
+	expect(newTracker.hasWorkdayStarted()).toStrictEqual(true);
+	expect(newTracker.hasBreakStarted()).toStrictEqual(false);
+});
+
+test("full work day with breaks test", () => {
+	vi.useFakeTimers();
+	vi.setSystemTime(new Date(2024, 4, 1, 8, 0, 0));
+	const tracker = createTracker(
+		{ ...defaultUserSettings, paidBreakDuration: 45 },
+		{ workdays: [] },
+	);
+
+	tracker.startWorkday();
+	expect(tracker.hasWorkdayStarted()).toStrictEqual(true);
+	expect(tracker.hasBreakStarted()).toStrictEqual(false);
+
+	// Two hours pass.
+	vi.advanceTimersByTime(1000 * 60 * 60 * 2);
+	expect(tracker.getTimeWorked()).toEqual({
+		hours: 2,
+		minutes: 0,
+		seconds: 0,
+	} as TimeWorked);
+
+	// Take a half-hour break.
+	tracker.startBreak();
+	vi.advanceTimersByTime(1000 * 60 * 30);
+	expect(tracker.getTimeWorked()).toEqual({
+		hours: 2,
+		minutes: 30,
+		seconds: 0,
+	} as TimeWorked);
+	tracker.endBreak();
+
+	// Half an hour passes.
+	vi.advanceTimersByTime(1000 * 60 * 30);
+	expect(tracker.getTimeWorked()).toEqual({
+		hours: 3,
+		minutes: 0,
+		seconds: 0,
+	} as TimeWorked);
+
+	// Take another 5 minute break
+	tracker.startBreak();
+	vi.advanceTimersByTime(1000 * 60 * 5);
+	tracker.endBreak();
+	expect(tracker.getTimeWorked()).toEqual({
+		hours: 3,
+		minutes: 5,
+		seconds: 0,
+	} as TimeWorked);
+
+	// Hour passes.
+	vi.advanceTimersByTime(1000 * 60 * 60);
+	expect(tracker.getTimeWorked()).toEqual({
+		hours: 4,
+		minutes: 5,
+		seconds: 0,
+	} as TimeWorked);
+
+	// Take 15 minute break, total break time 5 minutes over paid break amount.
+	tracker.startBreak();
+	vi.advanceTimersByTime(1000 * 60 * 15);
+	tracker.endBreak();
+	expect(tracker.getTimeWorked()).toEqual({
+		hours: 4,
+		minutes: 15,
+		seconds: 0,
+	} as TimeWorked);
+
+	// 4 hours pass.
+	vi.advanceTimersByTime(1000 * 60 * 60 * 4);
+	expect(tracker.getTimeWorked()).toEqual({
+		hours: 8,
+		minutes: 15,
+		seconds: 0,
+	} as TimeWorked);
+
+	// End workday.
+	tracker.endWorkday();
+	expect(tracker.getTimeWorked()).toEqual({
+		hours: 8,
+		minutes: 15,
+		seconds: 0,
+	} as TimeWorked);
+
+	// Wait another 4 hours after workday end, timeWorked should not change.
+	vi.advanceTimersByTime(1000 * 60 * 60 * 4);
+	expect(tracker.getTimeWorked()).toEqual({
+		hours: 8,
+		minutes: 15,
+		seconds: 0,
+	} as TimeWorked);
 });
--- src/lib/tracker.ts
+++ src/lib/tracker.ts
@@ -1,10 +1,15 @@
+import { differenceInSeconds } from "date-fns";
+
 type EventType = "start-workday" | "end-workday" | "start-break" | "end-break";
 
-interface Workday {
-	id: string;
+export interface WorkdayEvent {
+	time: Date;
+	type: EventType;
+}
+
+export interface Workday {
 	paidBreakDuration: number;
-	date: Date;
-	events: { time: Date; type: EventType }[];
+	events: WorkdayEvent[];
 }
 
 interface TrackingData {
@@ -17,11 +22,24 @@ export interface UserSettings {
 	paidBreakDuration: number;
 }
 
+export interface TimeWorked {
+	hours: number;
+	minutes: number;
+	seconds: number;
+}
+
 interface Tracker {
 	startWorkday(): void;
 	startBreak(): void;
 	endBreak(): void;
 	endWorkday(): void;
+	hasWorkdayStarted(): boolean;
+	hasBreakStarted(): boolean;
+	getTrackingData(): TrackingData;
+	/**
+	 * Gets the time worked for workday in progress or last full workday.
+	 */
+	getTimeWorked(): TimeWorked;
 }
 
 export function createTracker(
@@ -29,9 +47,171 @@ export function createTracker(
 	data: TrackingData,
 ): Tracker {
 	return {
-		startWorkday(): void {},
-		startBreak(): void {},
-		endBreak(): void {},
-		endWorkday(): void {},
+		startWorkday() {
+			if (hasWorkdayStarted(data)) {
+				throw new Error(
+					"Cannot start workday if current workday has not ended.",
+				);
+			}
+
+			data.workdays.push({
+				paidBreakDuration: settings.paidBreakDuration,
+				events: [{ type: "start-workday", time: new Date() }],
+			});
+		},
+		startBreak() {
+			if (!hasWorkdayStarted(data)) {
+				throw new Error("Workday has not started.");
+			}
+
+			const currentWorkday = getLastWorkday(data);
+
+			currentWorkday.events.push({
+				time: new Date(),
+				type: "start-break",
+			});
+		},
+		endBreak() {
+			const currentWorkday = getLastWorkday(data);
+			if (currentWorkday === undefined) {
+				throw new Error("Cannot end the break if a break has not started.");
+			}
+
+			const lastEvent = currentWorkday.events[currentWorkday.events.length - 1];
+
+			if (lastEvent.type !== "start-break") {
+				throw new Error("Cannot end the break if a break has not started.");
+			}
+
+			currentWorkday.events.push({ time: new Date(), type: "end-break" });
+		},
+		endWorkday() {
+			const lastWorkday = getLastWorkday(data);
+
+			if (lastWorkday === undefined) {
+				throw new Error("Cannot end the workday because it has not started.");
+			}
+
+			const lastEvent = lastWorkday.events[lastWorkday.events.length - 1];
+			if (lastEvent.type === "end-workday") {
+				throw new Error("Cannot end the workday because it has not started.");
+			}
+
+			lastWorkday.events.push({ time: new Date(), type: "end-workday" });
+		},
+		hasWorkdayStarted() {
+			return hasWorkdayStarted(data);
+		},
+		hasBreakStarted() {
+			const currentWorkday = getLastWorkday(data);
+			const lastEvent = currentWorkday.events[currentWorkday.events.length - 1];
+
+			return lastEvent.type === "start-break";
+		},
+		getTrackingData() {
+			return data;
+		},
+		getTimeWorked() {
+			const currentWorkday = getLastWorkday(data);
+			if (currentWorkday === undefined) {
+				return { hours: 0, minutes: 0, seconds: 0 };
+			}
+
+			if (currentWorkday.events.length === 1) {
+				const currentEvent = currentWorkday.events[0];
+				const secondsWorked = differenceInSeconds(
+					new Date(),
+					currentEvent.time,
+				);
+
+				return getTimeWorkedFromSecondsWorked(secondsWorked);
+			}
+
+			const secondsOnBreak = currentWorkday.events.reduce(
+				(secondsOnBreakSoFar, currentEvent, index, events) => {
+					const previousEvent = events[index - 1];
+					if (currentEvent.type === "end-break") {
+						secondsOnBreakSoFar += differenceInSeconds(
+							currentEvent.time,
+							previousEvent.time,
+						);
+					}
+
+					// If break is ongoing when getTimeWorked method is called we add the
+					// seconds elapsed since the start of the break.
+					if (
+						currentEvent.type === "start-break" &&
+						index === events.length - 1
+					) {
+						secondsOnBreakSoFar += differenceInSeconds(
+							new Date(),
+							currentEvent.time,
+						);
+					}
+
+					return secondsOnBreakSoFar;
+				},
+				0,
+			);
+
+			const secondsWorked =
+				currentWorkday.events.reduce(
+					(secondsWorkedSoFar, currentEvent, index, events) => {
+						const previousEvent = events[index - 1];
+						if (
+							currentEvent.type === "start-break" ||
+							currentEvent.type === "end-workday"
+						) {
+							secondsWorkedSoFar += differenceInSeconds(
+								currentEvent.time,
+								previousEvent.time,
+							);
+						}
+
+						// If the last event is end-break, measure time elapsed since it occurred.
+						if (
+							currentEvent.type === "end-break" &&
+							index === events.length - 1
+						) {
+							secondsWorkedSoFar += differenceInSeconds(
+								new Date(),
+								currentEvent.time,
+							);
+						}
+
+						return secondsWorkedSoFar;
+					},
+					0,
+				) + Math.min(currentWorkday.paidBreakDuration * 60, secondsOnBreak);
+
+			return getTimeWorkedFromSecondsWorked(secondsWorked);
+		},
 	};
+}
+
+function hasWorkdayStarted(data: TrackingData) {
+	const lastWorkday = getLastWorkday(data);
+
+	if (lastWorkday === undefined) {
+		return false;
+	}
+
+	const lastEvent = lastWorkday.events[lastWorkday.events.length - 1];
+	if (lastEvent === undefined) {
+		return false;
+	}
+
+	return lastEvent.type !== "end-workday";
+}
+
+function getLastWorkday(data: TrackingData): Workday {
+	return data.workdays[data.workdays.length - 1];
+}
+
+function getTimeWorkedFromSecondsWorked(secondsWorked: number): TimeWorked {
+	const hours = Math.floor(secondsWorked / (60 * 60));
+	const minutes = Math.floor((secondsWorked - hours * 60 * 60) / 60);
+	const seconds = secondsWorked - hours * 60 * 60 - minutes * 60;
+
+	return { hours: hours, minutes: minutes, seconds: seconds };
 }
